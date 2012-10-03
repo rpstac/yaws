7707937b469bf565fdea4abdaf00f63a236d6683
Rob Stacey <rob@robstacey.com>
2012-10-03 11:58:34 +0100

    Remove the test directory, these do not run when yaws is included as a dependancy in rebar, and update the reltool.confg to work with R14B04.

--- test/t2/flushtest.erl
+++
@@ -1,8 +0,0 @@
--module(flushtest).
--export([out/1]).
-
--include("../../include/yaws.hrl").
--include("../../include/yaws_api.hrl").
-
-out(_Arg) ->
-    [{status, 200}, {html, "break"}, flush, break].
